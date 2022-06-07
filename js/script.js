e5fe1c452e40d995ecb6443d6cb633c70bab9593
Eryk Szczepanek <szczepanekeryk@gmail.com>
2022-06-07 18:54:10 +0200

    finish blog script 6.3

--- js/script.js
+++ js/script.js
@@ -5,37 +5,37 @@
 //     const links = document.querySelectorAll('.titles a');
 //     console.log('links:', links);
 //   });
+{
+    const titleClickHandler = function(event) {
+        event.preventDefault();
+        const clickedElement = this;
+        console.log('Link was clicked!');
+    
+        const activeLinks = document.querySelectorAll('.titles a.active');
+        for (let activeLink of activeLinks) {
+            activeLink.classList.remove('active');
+        }
+    
+        clickedElement.classList.add('active')
+        console.log('clickedElement: ', clickedElement);
+    
+        const activeArticles = document.querySelectorAll('.posts article.active');
+        for (let activeArticle of activeArticles) {
+            activeArticle.classList.remove('active');
+        }
+    
+        const articleSelector = clickedElement.getAttribute('href')
+        console.log(articleSelector);
 
-const titleClickHandler = function(event) {
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
-  
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for (let activeLink of activeLinks) {
-        activeLink.classList.remove('active');
+        const targetArticle = document.querySelector(articleSelector)
+        console.log(targetArticle);
+
+        targetArticle.classList.add('active')
     }
-  
-
-    clickedElement.classList.add('active')
-    console.log('clickedElement: ', clickedElement);
-  
-    const activeArticles = document.querySelectorAll('.posts article.active');
-    for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active');
+    
+    const links = document.querySelectorAll('.titles a');
+    
+    for(let link of links){
+        link.addEventListener('click', titleClickHandler);
     }
-  
-    const articleSelector = clickedElement.getAttribute('href')
-    console.log(articleSelector);
-
-    const targetArticle = document.querySelector(articleSelector)
-    console.log(targetArticle);
-
-    targetArticle.classList.add('active')
 }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
