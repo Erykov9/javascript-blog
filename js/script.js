2ad8f755d44ea70fdb9a3968b047df8fb2a08999
Eryk Szczepanek <szczepanekeryk@gmail.com>
2022-06-15 08:22:26 +0200

    add eslint and edit script

--- js/script.js
+++ js/script.js
@@ -1,63 +1,62 @@
+/* eslint-disable no-inner-declarations */
 /* SCRIPT */
 'use strict';
 
 {
-    const titleClickHandler = function(event) {
-        event.preventDefault();
-        const clickedElement = this;
-        console.log('Link was clicked!');
+  const titleClickHandler = function(event) {
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('Link was clicked!');
     
-        const activeLinks = document.querySelectorAll('.titles a.active');
-        for (let activeLink of activeLinks) {
-            activeLink.classList.remove('active');
-        }
-    
-        clickedElement.classList.add('active')
-        console.log('clickedElement: ', clickedElement);
-    
-        const activeArticles = document.querySelectorAll('.posts article.active');
-        for (let activeArticle of activeArticles) {
-            activeArticle.classList.remove('active');
-        }
-    
-        const articleSelector = clickedElement.getAttribute('href')
-        console.log(articleSelector);
-
-        const targetArticle = document.querySelector(articleSelector)
-        console.log(targetArticle);
-
-        targetArticle.classList.add('active')
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
     }
     
-    const links = document.querySelectorAll('.titles a');
-    console.log(links);
+    clickedElement.classList.add('active');
+    console.log('clickedElement: ', clickedElement);
     
-    for(let link of links){
-        link.addEventListener('click', titleClickHandler);
+    const activeArticles = document.querySelectorAll('.posts article.active');
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
     }
+    
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
-    const optArticleSelector = '.post',
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    targetArticle.classList.add('active');
+  };
+    
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+    
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+
+  const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
-    function generateTitleLinks() {
-        const titleList = document.querySelector(optTitleListSelector);
-        const articles = document.querySelectorAll(optArticleSelector);
-        let html = ''
-        for (let article of articles) {
-            const articleId = article.getAttribute('id')
-            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-
-            html = html + linkHTML;
-            titleList.innerHTML = html;
-        }
-        const links = document.querySelectorAll('.titles a');
-        console.log(links);
-        
-        for(let link of links){
-            link.addEventListener('click', titleClickHandler);
-        }
+  function generateTitleLinks() {
+    const titleList = document.querySelector(optTitleListSelector);
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for (let article of articles) {
+      const articleId = article.getAttribute('id');
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      html = html + linkHTML;
+      titleList.innerHTML = html;
     }
-    generateTitleLinks();
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+    for(let link of links){
+      link.addEventListener('click', titleClickHandler);
+    }
+  }
+  generateTitleLinks();
 }
